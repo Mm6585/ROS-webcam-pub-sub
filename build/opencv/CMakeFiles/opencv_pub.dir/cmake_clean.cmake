773c349320ebe449539947952a2124ced21cac4b
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-04-02 13:08:57 +0900

    Add files via upload

---
+++ build/opencv/CMakeFiles/opencv_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_pub.dir/src/pub.cpp.o"
+  "/home/ldj/catkin_ws/devel/lib/opencv/opencv_pub.pdb"
+  "/home/ldj/catkin_ws/devel/lib/opencv/opencv_pub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/opencv/CMakeFiles/opencv_sub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_sub.dir/src/sub.cpp.o"
+  "/home/ldj/catkin_ws/devel/lib/opencv/opencv_sub.pdb"
+  "/home/ldj/catkin_ws/devel/lib/opencv/opencv_sub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
